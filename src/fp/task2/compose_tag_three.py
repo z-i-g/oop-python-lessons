560dea4bfa65fff67fb36bc44ac5a518c2849b76
Zagidullin Ayrat <zagidull007@gmail.ru>
2026-05-08 20:39:46 +0300

    Удалил комментарии

--- src/fp/task2/compose_tag_three.py
+++ src/fp/task2/compose_tag_three.py
@@ -6,15 +6,9 @@ def tag(tag_name, tag_attrs, tag_value):
     attrs_string = "".join([f' {k}="{v}"' for k, v in tag_attrs.items()])
     return f"<{tag_name}{attrs_string}>{tag_value}</{tag_name}>"
 
-# 1. Подготавливаем функции через частичное применение
-# Теперь bold_red и li_item — это функции, которые ждут только последний аргумент (value)
 bold_red = tag('b', {'style': 'color: red'})
 li_item = tag('li', {'class': 'list-group'})
 
-# 2. Создаем цепочку с помощью Compose
-# Текст сначала попадет в bold_red, а результат — в li_item
 complex_tag = Compose(bold_red).then(li_item)
 
-# 3. Проверка
 print(complex_tag("Hello"))
-# Результат: <li class="list-group"><b style="color: red">Внимание!</b></li>
