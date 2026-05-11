e58b97c19eda9adc2a6b33a29a91f6eb7d7f7c76
Zagidullin Ayrat <zagidull007@gmail.ru>
2026-05-11 21:13:09 +0300

    Добавил 3 задание по ФП

---
+++ src/fp/task3/functor.py
@@ -0,0 +1,14 @@
+from pymonad.tools import curry
+from pymonad.maybe import Maybe, Just, Nothing
+from pymonad.list import ListMonad
+
+@curry(2)
+def add(x, y):
+    return x + y
+
+add10_maybe = Maybe.apply(add).to_arguments(Just(10))
+print(add10_maybe.amap(Just(5)))
+print(add10_maybe.amap(Nothing))
+
+add10_list = ListMonad.apply(add).to_arguments(ListMonad(10))
+print(add10_list.amap(ListMonad(1, 2, 3)))
